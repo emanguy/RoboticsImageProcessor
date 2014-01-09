21e8f9fc16ae25e7888a715cb2e5111949ecdc82
Evan Rittenhouse <emanguy1@gmail.com>
2014-01-08 19:47:49 -0500

    Begin line detection

--- src/RunImageTest.java
+++ src/RunImageTest.java
@@ -1,6 +1,7 @@
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.highgui.Highgui;
@@ -9,6 +10,7 @@ import org.opencv.imgproc.Imgproc;
 public class RunImageTest {
 
 	private static final int EDGE_DETECT_SENSITIVITY = 300;
+	private static final int LINE_DETECT_SENSITIVITY = 50;
 	private static final int LUMINANCE_THRESHOLD = 252;
 	
     public static void main(String[] args) 
@@ -64,12 +66,31 @@ public class RunImageTest {
     	
     	//PERFORM EDGE DETECTION ON NEW IMAGE
     	
+    	//Create a new matrix for the edge detection
     	Mat edgeMap = new Mat();
     	Imgproc.Canny(luminanceChart, edgeMap, EDGE_DETECT_SENSITIVITY, EDGE_DETECT_SENSITIVITY);
     	Imgproc.blur(edgeMap, edgeMap, new Size(5,5));
     	Highgui.imwrite(generateFileCopyWithExtension(imageLocation, "Edge"), edgeMap);
     	
     	//PERFORM LINE DETECTION ON NEW IMAGE
+    	
+    	Mat lines = new Mat();
+    	Imgproc.HoughLinesP(edgeMap, lines, 1, Math.PI/180, LINE_DETECT_SENSITIVITY);
+    	
+    	//Iterate through each (x1, y1) (x2, y2) pair and draw the findings onto a new image
+    	int currentHue = 255;
+    	int hueChange = 127 / lines.cols();
+    	
+    	for (int line = 0; line < lines.cols(); line++)
+    	{
+    		//Draw the line
+    		Point lineStart = new Point(lines.get(0, line)[0], lines.get(0, line)[1]);
+    		Point lineEnd = new Point(lines.get(0, line)[2], lines.get(0, line)[3]);
+    		Core.line(edgeMap, lineStart, lineEnd, new Scalar(currentHue));
+    		
+    		currentHue -= hueChange; //Change the hue slightly for every line
+    	}
+    	
     	
     	//DRAW LINES ON NEW IMAGE
     	
